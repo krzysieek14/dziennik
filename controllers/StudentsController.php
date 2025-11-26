abff42dea394143b51ce2bab9c001a9200ea8e8d
= <=>
2025-11-26 11:31:10 +0100

    26.11 moja ciezka praca

--- controllers/StudentsController.php
+++ controllers/StudentsController.php
@@ -46,27 +46,12 @@ require_once "entities\Student.php";
             }
             include("templates/student/create.php");
         }
-        function edit()
-        {
-//            if (isset($_GET["form_sent"])){
-//                $db_connection = mysqli_connect("localhost", "root", "", "database");
-//
-//                $email = $_POST["email"]??"";
-//
-//                $query = mysqli_query($db_connection, "INSERT INTO `database`.users (`email`, `password`) VALUES ('$email', MD5('123123'))");
-//
-//                if ($query){
-//                    $user_id = mysqli_insert_id($db_connection);
-//                    $name = $_POST["name"]??"";
-//                    $surname = $_POST["surname"]??"";
-//                    $query = mysqli_query($db_connection, "INSERT INTO `database`.students (`name`, `surname`,`user_id`) VALUES ('$name', '$surname', '$user_id')");
-//                }
-//
-//
-//            }
+        function edit(?int $id=null){
+
 
 
             $db_connection = mysqli_connect("localhost", "root", "", "database");
+            $student_id = $id??$_GET['student_id'];
             $query = mysqli_query($db_connection,
                 "
                     SELECT 
@@ -77,11 +62,52 @@ require_once "entities\Student.php";
                     FROM `database`.students
                              JOIN `database`.users
                              ON `database`.`users`.`id` = `database`.`students`.`user_id`
-                             WHERE student_id = '{$_GET['student_id']}'");
+                             WHERE `database`.`students`.`id`= '{$student_id}'
+                                 
+                                 
+                    ;
 
 
             $student = mysqli_fetch_assoc($query);
-            include("templates/student/edit.php");
+            include('templates/student/edit.php');
         }
-        function delete(){}
+
+        function save(){
+        
+            print_r($_POST);
+            
+            $db_connection = mysqli_connect("localhost", "root", "", "database");
+            // Pobieranie danych
+            if(
+            isset($_POST["id"]) &&
+            isset($_POST["name"]) &&
+            isset($_POST["surname"])
+            }{
+                $sql = "
+                    UPDATE `database`.students
+                        ('name', 'surname')
+                SET
+                    `name` = '{$_POST["name"]}',
+                    `surname` = '{$_POST["surname"]}',
+                WHERE 
+                    `database`,`studenets`.`id` = '{$_POST["id"]}'
+
+            ";
+            
+            
+            
+
+                mysqli_query($db_connection, $sql);
+                $result = mysqli_affected_rows($db_connection);
+                if ($result==1) {
+                    return $this->index();
+                    }
+                }
+
+
+            return $this->edit($_POST['id']);
+        }
+
+
     }
+        function delete(){}
